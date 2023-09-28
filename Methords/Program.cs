23dab72ef70011d11f77862d0e7b5be46355755e
unknown <saummanzoor49@gmail.com>
2023-09-28 19:10:42 +0500

    Methords

---
+++ Methords/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace Learning{
+    class Methords
+    {
+        public static void Main(String[] args)
+        {
+            double a = -3.423;
+            // double b = Math.Pow(a, 2);
+            //double b = Math.Sqrt(a);
+            //double b = Math.Abs(a);
+            //double b = Math.Round(a);
+            // Ceiling, Floor, Max(x,y), Min(x,y) 
+            //double b = Math.Round(a);
+            //Random r = new Random();
+            //int randNum = r.Next(1, 7);
+
+            //int d = r.Next(1, 7) + 100;
+            //Console.WriteLine(d);
+
+            Console.WriteLine("Hello World");
+            Console.ReadKey(); 
+        }
+    }
+}
