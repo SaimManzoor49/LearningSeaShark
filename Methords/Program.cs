145dccb955116c06c38ebf8f60733a6b971cd025
unknown <saummanzoor49@gmail.com>
2023-09-29 22:46:39 +0500

    loops

--- Methords/Program.cs
+++ Methords/Program.cs
@@ -5,7 +5,7 @@ namespace Learning{
     {
         public static void Main(String[] args)
         {
-            double a = -3.423;
+            //double a = -3.423;
             // double b = Math.Pow(a, 2);
             //double b = Math.Sqrt(a);
             //double b = Math.Abs(a);
@@ -18,7 +18,56 @@ namespace Learning{
             //int d = r.Next(1, 7) + 100;
             //Console.WriteLine(d);
 
-            Console.WriteLine("Hello World");
+            //Console.WriteLine("Hello World");
+
+
+
+            ////////////////////////// Find Hypotenous//////////////////////////
+
+            //Console.WriteLine("Enter Side A");
+            //double a = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine("Enter Side B");
+            //double b = Convert.ToDouble(Console.ReadLine());
+
+
+            //double c = (Math.Pow(a, 2)) + (Math.Pow(b, 2));
+            //c = Math.Sqrt(c);
+
+            //Console.WriteLine("Ans:" + c);
+
+
+
+            //////////////////////Loops/////////////////////
+
+
+            //for(int i=0; i < 6; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+
+            //int i = 0;
+            //while(i<6)
+            //{
+            //   Console.WriteLine(i);
+            //    i++;
+            //}
+
+            //int i = 0;
+            //do
+            //{
+            //    Console.WriteLine(i);
+            //    i++;
+            //} while (i < 6);
+
+            //string[] i = { "0" , "1" , "2" , "3" , "4" , "5" };
+
+            //foreach (string c in i)
+            //{
+            //    Console.WriteLine(c);
+            //}
+
             Console.ReadKey(); 
         }
     }
