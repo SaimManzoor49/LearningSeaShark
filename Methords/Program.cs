be5cfc487bf4527d54da7732f4ddd72b9bbe3481
unknown <saummanzoor49@gmail.com>
2023-10-03 21:27:10 +0500

    little-bit-OOP

--- Methords/Program.cs
+++ Methords/Program.cs
@@ -1,87 +1,121 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using System.Net.Http.Headers;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
 
-namespace Learning{
-    class Methords
-    {
-        public static void Main(String[] args)
-        {
-            //double a = -3.423;
-            // double b = Math.Pow(a, 2);
-            //double b = Math.Sqrt(a);
-            //double b = Math.Abs(a);
-            //double b = Math.Round(a);
-            // Ceiling, Floor, Max(x,y), Min(x,y) 
-            //double b = Math.Round(a);
-            //Random r = new Random();
-            //int randNum = r.Next(1, 7);
-
-            //int d = r.Next(1, 7) + 100;
-            //Console.WriteLine(d);
-
-            //Console.WriteLine("Hello World");
+// namespace Learning{
+//  class Methords
+// {
+//    public static void Main(String[] args)
+//   {
 
 
+/*    int x = 3;
+    int y = 4;
+    int c;
 
-            ////////////////////////// Find Hypotenous//////////////////////////
+    Console.WriteLine('Multiplication Here');
+    c =x*y;
+    Console.WriteLine(c);
 
-            //Console.WriteLine("Enter Side A");
-            //double a = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine('Subtraction Here');
+    c = x - y;
+    Console.WriteLine(c);
 
-            //Console.WriteLine("Enter Side B");
-            //double b = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine('Addition Here');
+    c = x + y;
+    Console.WriteLine(c);
+
+    Console.WriteLine('Division Here');
+    c = x / y;
+    Console.WriteLine(c);
+*/
+
+//double a = -3.423;
+// double b = Math.Pow(a, 2);
+//double b = Math.Sqrt(a);
+//double b = Math.Abs(a);
+//double b = Math.Round(a);
+// Ceiling, Floor, Max(x,y), Min(x,y) 
+//double b = Math.Round(a);
+//Random r = new Random();
+//int randNum = r.Next(1, 7);
+
+//int d = r.Next(1, 7) + 100;
+//Console.WriteLine(d);
+
+//Console.WriteLine("Hello World");
 
 
-            //double c = (Math.Pow(a, 2)) + (Math.Pow(b, 2));
-            //c = Math.Sqrt(c);
 
-            //Console.WriteLine("Ans:" + c);
+////////////////////////// Find Hypotenous//////////////////////////
 
+//Console.WriteLine("Enter Side A");
+//double a = Convert.ToDouble(Console.ReadLine());
 
-
-            //////////////////////Loops/////////////////////
-
-
-            //for(int i=0; i < 6; i++)
-            //{
-            //    Console.WriteLine(i);
-            //}
+//Console.WriteLine("Enter Side B");
+//double b = Convert.ToDouble(Console.ReadLine());
 
 
-            //int i = 0;
-            //while(i<6)
-            //{
-            //   Console.WriteLine(i);
-            //    i++;
-            //}
+//double c = (Math.Pow(a, 2)) + (Math.Pow(b, 2));
+//c = Math.Sqrt(c);
 
-            //int i = 0;
-            //do
-            //{
-            //    Console.WriteLine(i);
-            //    i++;
-            //} while (i < 6);
+//Console.WriteLine("Ans:" + c);
 
-            //string[] i = { "0" , "1" , "2" , "3" , "4" , "5" };
 
-            //foreach (string c in i)
-            //{
-            //    Console.WriteLine(c);
-            //}
 
-            //////////////////////////Conditional Statments///////////////////
+//////////////////////Loops/////////////////////
 
-          //Random ran = new Random();
-          //int i = ran.Next(1,10);
 
-            //if (i > 5)
-            //{
-            //    Console.WriteLine("i>5 : i="+i);
-            //}else if (i < 5)
-            //{
-            //    Console.WriteLine("i<5 : i=" + i);
-            //}
+/*for (int i=0; i < 5; i++)
+{
+    Console.WriteLine("Saim\nUsman\nAli");
+} */
+
+
+
+//int i = 0;
+//while(i<6)
+//{
+//   Console.WriteLine(i);
+//    i++;
+//}
+
+//int i = 0;
+//do
+//{
+//    Console.WriteLine(i);
+//    i++;
+//} while (i < 6);
+
+//string[] i = { "0" , "1" , "2" , "3" , "4" , "5" };
+
+//foreach (string c in i)
+//{
+//    Console.WriteLine(c);
+//}
+
+
+
+
+
+
+
+
+//////////////////////////Conditional Statments///////////////////
+
+/* Random ran = new Random();
+int i = ran.Next(1,10);
+
+if (i > 5)
+{ 
+    Console.WriteLine("i>5 : i="+i);
+}else if (i < 5)
+{
+    Console.WriteLine("i<5 : i=" + i);
+} */
+
 
 /*            switch (i)
             { 
@@ -110,7 +144,80 @@ namespace Learning{
             }
                     */
 
-            Console.ReadKey(); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Console.ReadKey(); 
+//   }
+//}
+
+
+
+
+
+//}
+
+
+
+
+
+//////////////////// Little Bit OOP////////////////
+
+
+
+
+namespace OOP
+{
+    public class Concat
+    {
+
+        string s1;
+        string s2;
+        string s3 = "";
+
+        public Concat(string s1, string s2)
+        {
+            this.s1 = s1;
+            this.s2 = s2;
+        }
+
+        public void mergeStrings()
+        {
+            this.s3 = string.Concat(s1, s2);
+        }
+
+        public void showString()
+        {
+            Console.WriteLine(this.s3);
+        }
+
+
+    }
+
+    class MAIN
+    {
+        public static void Main(string[] args)
+        {
+            Concat s = new Concat("Saim ", "Manzoor");
+
+            s.mergeStrings();
+            s.showString();
+
+
+            Console.ReadKey();
+
         }
     }
+
+
 }
